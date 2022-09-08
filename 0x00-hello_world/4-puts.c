c5376ad980af19d10912a773f652a746a083d926
Vicsituma <vicsituma45@gmail.com>
2022-09-08 18:15:41 +0000

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - prints a message
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
