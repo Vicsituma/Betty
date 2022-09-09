250d8fb300b1d54b27a47111d378975e51f7567d
Vicsituma <vicsituma45@gmail.com>
2022-09-09 22:01:42 +0000

    uppercase and lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,11 @@ int main(void)
 	for (c = 'a'; c <= 'z'; ++c)
 {
 		putchar(c);
-		putchar(toupper(c));
 }
-	putchar('\n');
+	for (c = 'A'; c <= 'Z'; ++c)
+{
+		putchar(c);
+}
+		putchar('\n');
 	return (0);
 }
