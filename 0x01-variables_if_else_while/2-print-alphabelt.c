934c9f1fae7e93dc8155e8c270148c1d4ea1d9cb
Vicsituma <vicsituma45@gmail.com>
2022-09-09 21:09:58 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/2-print-alphabelt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - program to print alphabet letters
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++C)
+		putchar(c);
+	return (0);
+}
