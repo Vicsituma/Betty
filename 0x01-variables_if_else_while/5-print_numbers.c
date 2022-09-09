3859a9aa1d510a5c583710b02919620852d3a011
Vicsituma <vicsituma45@gmail.com>
2022-09-09 22:40:05 +0000

    numbers to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - program that prints numbers 0 to 9
+*Return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 0
+	while (i < 10)
+{
+		printf("%d", i);
+		i++;
+}
+	return (0);
+}
