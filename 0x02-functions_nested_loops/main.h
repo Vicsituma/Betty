1c37ddccc71a40c5c99998a4a6e88c1d96096631
Vicsituma <vicsituma45@gmail.com>
2022-09-14 05:37:20 +0000

    starting on nested functions

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* File - main.h
+* Desc: a header file containing function prototypes for 0x02-functions_nested_loops dir
+*/
+
+int _putchar(char c);
+
+
+
+#endif
