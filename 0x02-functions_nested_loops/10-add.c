dc05816931f80937a99801677dc6293222329e1d
Vicsituma <vicsituma45@gmail.com>
2022-09-14 18:27:08 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* add - adds two integers
+* @n: the first integer
+* @i: the second integer
+* Return:  an int(result)
+*/
+int add(int n, int i)
+{
+	int result;
+
+	result = n + i
+	_putchar(result + '0')
+	return result
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* times_table - prints the 9 times table, starting with 0
+* Return: none
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+{
+		_putchar(
