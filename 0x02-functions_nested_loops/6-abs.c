5d9e83722cde2eba249402b1d166f685f57cb322
Vicsituma <vicsituma45@gmail.com>
2022-09-14 12:29:48 +0000

    islower

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - prints absolute value of n integer
+* @n: number to check
+* Return: always 0
+*/
+int _abs(int i)
+{
+	if (i < 0)
+		_putchar(i);
+	else
+		_putchar(i);
+	return (0);
+}
