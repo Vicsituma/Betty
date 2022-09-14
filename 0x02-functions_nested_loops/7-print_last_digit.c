7edd0c19f01f2ea578548a2b6caf0342078375de
Vicsituma <vicsituma45@gmail.com>
2022-09-14 13:16:41 +0000

    the lst digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit -returns value of the last digit
+* @n: number to get last digit
+*Return: the last digit
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
