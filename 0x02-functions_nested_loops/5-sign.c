cf6fd14d6d17e840d7ebd147f41057f58e56413d
Vicsituma <vicsituma45@gmail.com>
2022-09-14 10:10:23 +0000

    numbersign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - prints the sign of a number
+* @n: a number to check whether ositive or negative
+*Return: 1 (for +), 0 (for zero), -1 (for -)
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+{
+		_putchar('-');
+		return (-1);
+}
+	else if (n > 0)
+{
+		_putchar('+');
+		return (1);
+}
+	else
+{
+		return (0);
+}
+}
