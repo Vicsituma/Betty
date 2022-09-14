fc27d5b471374144bce461c2caca56fb2e7d287e
Vicsituma <vicsituma45@gmail.com>
2022-09-14 19:01:03 +0000

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,16 +3,17 @@
 /**
 * print_to_98 - prints all natural numbers from n to 98
 * @n: initial number
-* Return: Always 0
+* Return: none
 */
 void print_to_98(int n)
 {
-	for (int i = n; i <= 98; i++)
+	int i = n;
+
+	for (i = n; i <= 98; i++)
 {
 		_putchar((i + '0'));
 		_putchar(',');
 		_putchar(' ');
 }
 	_putchar('\n');
-	return (0);
 }
