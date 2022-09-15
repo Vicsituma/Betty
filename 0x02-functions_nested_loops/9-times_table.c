7924a474cbf17f625d46823f654249cf2d24539f
Vicsituma <vicsituma45@gmail.com>
2022-09-15 03:43:42 +0000

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,26 +15,12 @@ void times_table(void)
 {
 		for (j = 0; j <= n; j++)
 {
-			_putchar(' ');
 			k = i * j;
 
-			if (k <= 99)
-				_putchar(' ');
-			if (k <= 9)
-				_putchar(' ');
-			if (k >= 100)
-			{
-				_putchar((k / 100) + '0');
-				_putchar((k / 10) % 10 + '0');
-			}
-			else if (k <= 99 && k >= 10)
-			{
-				_putchar((k / 10) + '0');
-			}
+			_putchar((k % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 
-			_putchar((k % 10) + '0');
 }
 		_putchar('\n');
 }
