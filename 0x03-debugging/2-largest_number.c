e8c0c66ebdae9e264974dfd41b4cc601bbdaf40e
Vicsituma <vicsituma45@gmail.com>
2022-09-15 13:45:01 +0000

    largest no

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -21,7 +21,7 @@ int largest_number(int a, int b, int c)
 	else if ((c > a && c > b) || (c > a && c == b))
 	        largest = c;
 	
-	else (a == b && a == c)
+	else
 		largest = a;
 
 	return (largest);
