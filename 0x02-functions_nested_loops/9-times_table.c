589470328a52f8443e5060fbaaacad0566ebaa21
Vicsituma <vicsituma45@gmail.com>
2022-09-15 03:10:20 +0000

    tiimetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,13 +2,42 @@
 #include "main.h"
 /**
 * times_table - prints the 9 times table, starting with 0
+* @n: parameter for product
 * Return: none
 */
-void times_table(void)
+void times_table(int n)
 {
 	int i;
 	int j;
+	int k;
 
-	for (i = 0; i <= 9; i++)
+	if (n >= 0 && n <= 15)
 {
-		_putchar(
+		for (i = 0; i <= 9; i++)
+{
+			_putchar('0');
+			for (j = 0; j <= n; j++)
+{
+				_putchar(',');
+				_putchar(' ');
+				k = i * j;
+
+				if (k <= 99)
+					_putchar(' ');
+				if (k <= 9)
+					_putchar(' ');
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) % 10 + '0');
+				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + '0');
+				}
+				_putchar((k % 10) + '0');
+}
+			_putchar('\n');
+}
+}
+}
