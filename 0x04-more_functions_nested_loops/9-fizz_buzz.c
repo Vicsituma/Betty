4d22f3d09789a01e7f0b0eec3dde9f2cea80b294
Vicsituma <vicsituma45@gmail.com>
2022-09-16 21:57:56 +0000

    fizzb

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,11 +7,9 @@ void main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
-		if (i == 100)
-			printf("Buzz");
-		else if ((i % 15) == 0)
+		if ((i % 15) == 0)
 			printf("FizzBuzz ");
 		else if ((i % 3) == 0)
 			printf("Fizz ");
@@ -20,6 +18,7 @@ void main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("Buzz");
 	printf("\n");
 }
 
