977a79b455177347f6fab6a9ca69f75ae8d0de76
Vicsituma <vicsituma45@gmail.com>
2022-09-16 18:15:23 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,14 +7,9 @@
  */
 int _isupper(int c);
 {
-	char i;
-
-	for (i = 'A'; i <= 'Z'; i++)
-	{
-		if (c == i)
-			return (1);
-		else
-			return (0);
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
 
