c5b3e6b87b2d8b0786f8760a4f29866038c680fb
Vicsituma <vicsituma45@gmail.com>
2022-09-16 20:08:20 +0000

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,18 @@
 */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < 15; i++)
+	for (j = 0; j < 10; j++)
 	{
-		if (i < 10)
-			_putchar(i + '0');
-		else if (i >= 10)
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+		for (i = 0; i < 15; i++)
+		{
+			if (i < 10)
+				_putchar(i + '0');
+			else if (i >= 10)
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
