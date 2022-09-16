2e36f1230a22fef19ce0582d1c9f2b3172a6546f
Vicsituma <vicsituma45@gmail.com>
2022-09-16 19:59:37 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - prints the numbers 0 to 14
+ * followed by a new line
+ * Return: none
+*/
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		if (i < 10)
+			_putchar(i + '0');
+		else if (i >= 10)
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+	}
+	_putchar('\n');
+}
