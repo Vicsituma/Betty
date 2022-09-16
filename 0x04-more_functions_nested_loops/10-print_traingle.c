59c8f0945a19452930f5165cf38112ffd8341083
Vicsituma <vicsituma45@gmail.com>
2022-09-16 22:44:44 +0000

    traingle

---
+++ 0x04-more_functions_nested_loops/10-print_traingle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_traingle - prints a triangle followed by new line.
+ * @size - size of triangle
+*/
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = (size - i); j > 1; j--)
+			_putchar(' ');
+		for (k = (i + j); k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
