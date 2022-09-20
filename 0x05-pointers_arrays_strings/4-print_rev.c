baaab4a0cceccfd70b6d41c5d6f772da5e4ce197
Vicsituma <vicsituma45@gmail.com>
2022-09-20 04:49:34 +0000

    reversing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,10 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = (_strlen(s) - 1); i >= 0; i--)
+	 while (*s++)
+		 i++;
+	 for (i = (i - 1); i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
-}
+
+
