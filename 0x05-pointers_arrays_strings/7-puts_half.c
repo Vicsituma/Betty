3dc9784d2ff5374a30dde6b49c948b38be92647c
Vicsituma <vicsituma45@gmail.com>
2022-09-20 17:39:24 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half -  prints half of a string, followed by a new line.
+ * @str: string to print
+*/
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j <= i / 2; j++)
+	{
+		_putchar(str[j]);
+	}
+}
+
---
+++ 0x05-pointers_arrays_strings/count.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int i;
+	char t[]= "school";
+
+	for (i = 0; t[i] != '\0';i++)
+	{
+	}
+	printf("%d\n", i);
+}
