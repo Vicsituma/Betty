296aeb5460195b6b63473ca7278eedd9ffbabec3
Vicsituma <vicsituma45@gmail.com>
2022-09-21 00:11:17 +0000

    arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n - 1; i++)
 	{
+		printf("%d",a[i]);
 		if (i == n - 1)
 		{
-			printf("%d\n", a[i]);
-			break;
+			continue;
 		}
-		printf("%d, ", a[i]);
+		printf(", ");
 	}
+	printf("\n");
 }
