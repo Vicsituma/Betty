0a25f070fa0f02bfa85f8562f03d7d39db4c8923
Vicsituma <vicsituma45@gmail.com>
2022-09-22 18:13:04 +0000

    concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,7 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
+
 	while (dest[i] != '\0')
 	{
 		++i;
@@ -19,5 +20,5 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 	}
 	dest[i] = '\0';
-	return (&dest);
+	return (dest);
 }
