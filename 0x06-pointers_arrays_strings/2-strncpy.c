713cde2f49b7025d81abcb85d92c1fc3e4fff62e
Vicsituma <vicsituma45@gmail.com>
2022-09-23 02:20:20 +0000

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,15 +6,14 @@
  * @n: where to paste in dest
  * Return: pointer to the resulting string dest
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i, rem, j;
 
-	i = 0;
 	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
-	
-	return dest;
+
+	return (dest);
 }
