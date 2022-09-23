c0120b6306a3a5b235d448044e62be1cd78fc545
Vicsituma <vicsituma45@gmail.com>
2022-09-23 11:37:14 +0000

    correction

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * numcomb - prints all possible different combinations of two digits
+ * Return: none
+*/
+void numcomb(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			else if ( j > i)
+			{
+				continue
+			}
+			else if
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+			}
+			putchar(', '
+		}
+	}
+}
