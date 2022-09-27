7ba9899df2b784c817e1246ff79e31e8dd9bb363
Vicsituma <vicsituma45@gmail.com>
2022-09-27 07:38:04 +0000

    even more

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * main - a header file.
+ * Desc: a header for functions in projects for 0x07-even more pointers
+ * arrays and strings
+*/
+
+#include <stdio.h>
+
+char *_memset(char *s,char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int_strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+
+#endif
+
