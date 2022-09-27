342d7f42a51737fea31d8948bfa820cad16a9a5c
Vicsituma <vicsituma45@gmail.com>
2022-09-27 06:42:15 +0000

    007

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {	
-	int marks[10], i, n, sum = 0;
-	double average;
+	int num = 123;
+	int *pr2;
+	int **pr1;
 
-	printf("Enter number of elements: ");
-	scanf("%d", &n);
+	pr2 = &num;
+	pr1 = &pr2;
+
+	printf("\n value of num is: %d", num);
+	printf("\n value of num using pr2 is: %d", *pr2);
+	printf("\n value of num using pr1 is: %d", **pr1);
 	
-	for (i = 0; i < n; ++i)
-	{
-		printf("Enter number %d: ", i + 1);
-		scanf("%d", &marks[i]);
-		sum += marks[i];
-	}
-	average = (double) sum / n;
-	printf("Average = %.21f", average);
+	printf("\n Address of num is: %p", &num);
+	printf("\n Address of num using pr2 is: %p", pr2);
+	printf("\n Address of num using pr1 is: %p", *pr1);
+
+	printf("\n value of Pointer pr2 is: %p:", pr2);
+	printf("\n value Pointer pr2 using pr1 is: %p", *pr1);
+
+	printf("\n address of pointer pr2 is: %p", &pr2);
+	printf("\n address of pointer pr2 using pr1 is: %p", pr1);
+
+	printf("\n value of pointer pr1 is: %p", pr1);
+	printf("\n address of pointer pr1 is: %p", &pr1);
+
 	return (0);
 }
