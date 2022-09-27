90d64d15d8f42cc702dc5f183d78bf80abb492ec
Vicsituma <vicsituma45@gmail.com>
2022-09-27 10:37:32 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - copies memory area.
+ * @dest: memory area to copy to
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ * Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i, j;
+	int k = n;
+
+	for (i = 0; i < k; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
