05afd6db001368cc13553d3d91e3176c33232ecf
Vicsituma <vicsituma45@gmail.com>
2022-09-29 10:00:12 +0000

    correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,16 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
-	int j;
-	while (*s++)
-		i++;
+	int i;
 
-	for (j = 0; j <= i; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(s[j]);
+		_putchar(*s[i]);
 	}
 	_putchar('\n');
-	for (; i >= 0; i--)
+	for (i = (i - 1); i >= 0; i--)
 	{
-		_putchar(s[i]);
+		_putchar(*s[i]);
 	}
 }
 
