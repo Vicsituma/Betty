02ed03a68f5da66898bcd360456a4e900869a696
Vicsituma <vicsituma45@gmail.com>
2022-09-29 20:20:33 +0000

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * factorial -  returns the factorial of a given number.
+ * @n: an integer to for factorials
+ * Return: the factorial
+*/
+int factorial(int n)
+{
+	int i;
+
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	
+	i = n * factorial(n-1);
+	return (i);
+}
