c90d173fc6525df9d3f1e5276ef4da44d4ee3ed0
Vicsituma <vicsituma45@gmail.com>
2022-09-30 20:13:01 +0000

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * whatsmyname -  prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: array containing the arguments
+ * Returns: 0
+ */
+int whatsmyname(int argc, char *argv[])
+{
+	printf("%s\n", argv);
+
+	return (0);
+}
