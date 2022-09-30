a312bcf4d6cef2290458481d231a2f03d1edd941
Vicsituma <vicsituma45@gmail.com>
2022-09-30 08:22:32 +0000

    primes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,30 @@
 #include "main.h"
 /**
+ * a_prime: checks divisibilty of n by lesser numbers if n>1
+ * @n: potential prime_number
+ * @i: a divisor
+ * Return:  0 if n%i=0 else 1
+*/
+int a_prime(int n, int i)
+{
+	if (i == n && n > 1)
+		return 1;
+	if (n % i == 0)
+		return (0);
+	else
+		return(a_prime(n, i +1);
+}
+
+
+
+/**
  * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0
  * @n: integer to check
  * Return: 1 if the input integer is a prime number, otherwise return 0
 */
+
+
 int is_prime_number(int n)
 {
-	if (n == 1)
-		return (1);
-	if (n % is_prime_number(n - 1) == 0)
-			return (0);
+	return (a_prime(n, 2));
 }
