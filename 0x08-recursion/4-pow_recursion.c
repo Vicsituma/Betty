f90ceca45c668fbdd1a040a6b7156d615df3ef36
Vicsituma <vicsituma45@gmail.com>
2022-09-30 05:13:52 +0000

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,10 +7,12 @@
 */
 int _pow_recursion(int x, int y)
 {
-	if (x < 1 || y < 1)
-		return (-1);
+	if (y < 0 )
+		return (-1)
+	if (y == 0)
+		return (1);
 	if (y == 1)
 		return (x);
-	x = x * _pow_recursion(x, y-1);
+	x = x * _pow_recursion(x, y - 1);
 	return (x);
 }
