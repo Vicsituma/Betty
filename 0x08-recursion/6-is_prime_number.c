e6dbf34a34b794a5d3f68fe3d2d5723cdd6e2bb4
Vicsituma <vicsituma45@gmail.com>
2022-09-30 08:42:08 +0000

    primo

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,10 +2,10 @@
 int a_prime(int n, int i);
 int is_prime_number(int n);
 /**
- * a_prime: checks divisibilty of n by lesser numbers if n>1
- * @n: potential prime_number
+ * a_prime: checks divisibilty of n by lesser numbers of n
+ * @n: potential prime number
  * @i: a divisor
- * Return:  0 if n%i=0 else 1
+ * Return:  0 else 1
 */
 int a_prime(int n, int i)
 {
@@ -13,7 +13,7 @@ int a_prime(int n, int i)
 		return (0);
 	if (i == n && n > 1)
 		return (1);
-	if (n % i == 0 )
+	if (n % i == 0)
 		return (0);
 	else
 		return (a_prime(n, i + 1));
