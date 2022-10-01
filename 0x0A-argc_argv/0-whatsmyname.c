75cde1e4bb7f003014d377bb60bfb77e18154ea5
Vicsituma <vicsituma45@gmail.com>
2022-10-01 16:37:26 +0000

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,11 @@
  * main -  prints its name, followed by a new line.
  * @argc: argument count
  * @argv: array containing the arguments
- * Returns: 0
+ * Return: 0
  */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
