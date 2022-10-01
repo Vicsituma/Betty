692ec203c57377059e110649b30da7b6d1cd510e
Vicsituma <vicsituma45@gmail.com>
2022-10-01 18:55:13 +0000

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: argument count
+ * @arg: array of arguments
+ * Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
