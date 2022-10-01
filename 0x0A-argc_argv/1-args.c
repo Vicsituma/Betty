07a0646b6e13755cc3eaede526693d36b2240c42
Vicsituma <vicsituma45@gmail.com>
2022-10-01 16:42:24 +0000

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,12 @@
 #include "main.h"
 /**
- * args - prints the number of arguments passed into it.
- * 
+ * main - prints the number of arguments passed into it.
+ * @argc: number of arguments passed to func
+ * @argv: array of arguments
+ * Return: always 0
+*/
+int main(int argc, char  __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
