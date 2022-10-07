16af95a81b8574106169d2716c9a920a49eb16d0
Vicsituma <vicsituma45@gmail.com>
2022-10-07 21:43:29 +0000

    callocs

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * *_calloc - allocates memory for an array, using malloc.
  * @nmemb: no of elements in array
@@ -8,8 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *arr;
-	int i;
-
+	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
