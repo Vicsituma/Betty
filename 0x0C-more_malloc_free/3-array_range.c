fdb6d5e9bb3d92ec57f759b0532357656cbd5aa9
Vicsituma <vicsituma45@gmail.com>
2022-10-07 20:37:50 +0000

    realloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *array_range -  creates an array of integers.
+ * @min: minimum integer
+ * @max: maximum integer
+ * Return:the pointer to the newly created array
+*/
+int *array_range(int min, int max)
+{
+	int *arr, i;
+
+	if (min > max)
+		return (NULL);
+	i = max - min;
+	arr = malloc(sizeof(int) * (i + 1));
+	if (!arr)
+		return (NULL);
+	j = 0;
+	for (i = min; i <= max; i++)
+		arr[j++] = i;
+	return(arr);
+
+}
