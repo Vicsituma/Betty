f6542d2cb51cace9fd29efcd9a90ab773ba9d7c7
Vicsituma <vicsituma45@gmail.com>
2022-10-11 01:30:51 +0000

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits.
+ * Return: always 0
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; j < 10; k++)
+			{
+				if (i != j && i != k && j != k && i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+				}
+				else
+					continue;
+				if (i == 7 && j == 8 && k == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
