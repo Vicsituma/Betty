3e15e3bf3c3ac880e4a3653dcb35bf5f0408fd9d
Vicsituma <vicsituma45@gmail.com>
2022-10-11 13:45:23 +0000

    dog init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog -initializes a variable of type struct dog
+ * @d: a dog instance
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: none
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
