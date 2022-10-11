e9577b581accec47624f394d7da09eef21b82886
Vicsituma <vicsituma45@gmail.com>
2022-10-11 23:01:56 +0000

    dogt

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include "dog.h"
+/**
+ * _strlen - a function thatgets a length of string
+ * @str: the string to get the length
+ * Return: string length
+ */
+int _strlen(const char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
+
+/**
+ * _strcpy -a function that returns @dest with a copy of a
+ * string from @src
+ * @src: string to copy
+ * @dest: where to paste
+ * Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * *new_dog -  creates a new dog.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's ower
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = _strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcpy(dog->owner, owner);
+	return (dog);
+}
+
+}
