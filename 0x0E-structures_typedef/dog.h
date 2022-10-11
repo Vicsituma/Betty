824c0e8262934ed409c734ee77d06ece9fbb8aa8
Vicsituma <vicsituma45@gmail.com>
2022-10-11 22:05:09 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,8 @@ struct dog
 	char *name;
 	char *owner;
 	float age;
-}dog_t;
+};
+typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
