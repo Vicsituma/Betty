7735c769a6a477982ab1bda22c5e77cf1d61734e
Vicsituma <vicsituma45@gmail.com>
2022-10-11 14:08:25 +0000

    dog init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,11 +7,8 @@
  * @owner: dog owner
  * Return: none
 */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
 	d -> name = name;
 	d -> age = age;
 	d -> owner = owner;
