adde97a8fa779aedaaf4501c3d845819dca30900
Vicsituma <vicsituma45@gmail.com>
2022-10-11 19:54:54 +0000

    file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line
+ * Return: always 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+}
