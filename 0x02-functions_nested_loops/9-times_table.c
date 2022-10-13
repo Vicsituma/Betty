d813e88cfc1b5ab63efdba9c44ca4ade1eea4b5b
Vicsituma <vicsituma45@gmail.com>
2022-10-13 03:41:58 +0000

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,17 +16,14 @@ void times_table(void)
 		_putchar(0 + '0');
 		for (j = 1; j <= n; j++)
     		{
-
 			_putchar(',');
 			_putchar(' ');
 			k = i * j;
+			if (k < 9)
+				_putchar(' ');
 			if (k >= 10)
 			{
 				_putchar((k / 10) + '0');
-			}
-			if (k < 9)
-			{
-				_putchar(' ');
 			}
 			_putchar((k % 10) + '0');
 		}
