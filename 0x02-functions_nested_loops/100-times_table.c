50eac21535055dbf1ee909a1fc2a15963e62cab0
Vicsituma <vicsituma45@gmail.com>
2022-10-13 05:31:59 +0000

    100times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	
+
 	for (i = 0; i <= n; i++)
 	{
 		if (n > 15 || n < 0)
@@ -32,7 +32,7 @@ void print_times_table(int n)
 			else
 			{
 				_putchar((k / 100) + '0');
-				_putchar(((k-((k / 100) * 100)) / 10) + '0');
+				_putchar(((k - ((k / 100) * 100)) / 10) + '0');
 			}
 			_putchar((k % 10) + '0');
 		}
