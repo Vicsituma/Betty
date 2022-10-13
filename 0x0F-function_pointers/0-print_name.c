d80db9769750ac9a6806aef5662fc3b2dc2fea22
Vicsituma <vicsituma45@gmail.com>
2022-10-13 18:14:14 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: pointer to a character variable
+ * @f: a funtion pointer
+ * Return: none
+*/
+void print_name(char *name, void (*f)(char *));
+{
+	f(*name);
+}
