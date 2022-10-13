dec701dbbb1a00c505ea81588a3e9750b19d2fd6
Vicsituma <vicsituma45@gmail.com>
2022-10-13 18:53:40 +0000

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (size < 1 || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 	for (i = 0; i < (int)size; i++)
 	{
