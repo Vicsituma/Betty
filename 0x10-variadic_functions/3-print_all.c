b14496a0cb5a4a1741694c9246987375aedc2610
Vicsituma <vicsituma45@gmail.com>
2022-10-17 19:27:03 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,40 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i;
+	int i;
+	size_t counter = 0;
+	char c, temp, *str;
+	float f;
 
+	va_start(ap, format);
+	while (format && counter < strlen(format))
+	{
+		temp = *(format + counter);
+		switch (temp)
+		{
+			case 'c':
+				c = va_arg(ap, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				c = va_arg(ap, int);
+				printf("%d", i);
+				break;
+			case 's':
+				str = va_arg(ap, char*);
+				if (!str)
+					str = "(nil)";
+				printf("%c", str);
+				break;
+			case 'f':
+				f = va_arg(ap, double);
+				printf("%f", f);
+				break;
+		}
+		if ((temp == 'c' || temp == 'i' || temp == 's' || temp == 'f') &&
+				counter < strlen(format) - 1)
+			printf(", ");
+		counter++;
+	}
 	printf("\n");
 }
