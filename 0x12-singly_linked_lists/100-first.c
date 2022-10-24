950cf15f7b585a09535e05b3c45907443d08d708
Vicsituma <vicsituma45@gmail.com>
2022-10-24 20:47:54 +0000

    print this before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,10 @@
  * printfirst - prints before main function.
  *
  */
-void printfirst() __attribute__((constructor));
+void printfirst(void) __attribute__((constructor));
 
 void printfirst(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
