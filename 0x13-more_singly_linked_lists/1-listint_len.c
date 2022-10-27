c6cdb41d57779bfc78dc2e248382c98ef967cf88
Vicsituma <vicsituma45@gmail.com>
2022-10-27 02:56:02 +0000

    list lenght

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - prints all the elements of a list_t list.
+ * @h: a singly linked list
+ * Returns: number of elements in a linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i += 1;
+	}
+	return (i);
+}
