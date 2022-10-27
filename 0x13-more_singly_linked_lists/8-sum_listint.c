9f043d6636a0e37c20546ceb4d99347952aba27c
Vicsituma <vicsituma45@gmail.com>
2022-10-27 17:10:03 +0000

    sum  data (n) in linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * sum_listint - returns the sum of all the data (n)
- * of a listint_t linked list. 
+ * of a listint_t linked list.
  * @head: pointer to a list
  * Return: sum of data(n) in list
  */
