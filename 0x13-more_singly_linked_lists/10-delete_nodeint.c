462072361da366af7a079f27d880520517990340
Vicsituma <vicsituma45@gmail.com>
2022-10-30 16:51:25 +0000

    delete a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,10 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		if ((*head)->next != NULL)
-			*head = (*head)->next;
-		else
-			*head = NULL;
+		*head = (*head)->next;
 		free(temp);
 		return (1);
 
@@ -43,5 +40,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	free(temp2);
-	return (-1);
+	return (1);
 }
