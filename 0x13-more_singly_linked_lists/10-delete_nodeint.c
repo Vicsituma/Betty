4de37af58c25e0317f0944af2367983da2ac9d58
Vicsituma <vicsituma45@gmail.com>
2022-10-30 06:57:10 +0000

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		if ((*head)->next != NULL)
+			*head = (*head)->next;
 		return (1);
 	}
 	for (i = 0; i < (index - 1); i++)
