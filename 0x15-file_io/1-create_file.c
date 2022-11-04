e71d35d1bf8aaf804bd5f7c05edd1bb626f08f8d
Vicsituma <vicsituma45@gmail.com>
2022-11-04 10:13:11 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = strlen(text_content);
+	int o, w, len = strlen(text_content) - 1;
 
 	if (filename == NULL)
 		return (-1);
