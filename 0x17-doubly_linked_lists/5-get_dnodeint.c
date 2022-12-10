6a03e1fbc6da30f00d6b7303e6d596de9b8d53ff
Vicsituma <vicsituma45@gmail.com>
2022-12-10 02:11:17 -0800

    get node debug

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,11 +3,11 @@
  * *get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
  * @head: pointer to a dlistint_t list
  * @index: index of node to return, starting from 0
- * Return: nth index
+ * Return: nth node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	int n = 0;
+	unsigned int n = 0;
 
 	if (!head)
 		return (NULL);
