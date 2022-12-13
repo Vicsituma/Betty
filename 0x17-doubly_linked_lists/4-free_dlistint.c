dcd2f79ead79907b8a921825924e7d82c1b27aac
Vicsituma <vicsituma45@gmail.com>
2022-12-12 18:29:52 -0800

    debug

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -11,7 +11,7 @@ void free_dlistint(dlistint_t *head)
 	while (head)
 	{
 		temp = head;
-		head = head-next;
+		head = head->next;
 		free(temp);
 	}
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -30,15 +30,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		tmp = tmp->next;
 		i++;
 	}
-	if (tmp->next == NULL && i < index)
-		return (-1);
 	if (tmp->next == NULL && i == index)
 	{
 		tmp->prev->next = NULL;
 		free(tmp);
 		return (1);
 	}
-	else
+	else if (tmp->next && i == index)
 	{
 		tmp->next->prev = tmp->prev;
 		tmp->prev->next = tmp->next;
