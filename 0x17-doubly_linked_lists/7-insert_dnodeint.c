f2534cbe725c825082b2736345ba2d30a0cfb336
Vicsituma <vicsituma45@gmail.com>
2022-12-12 17:28:04 -0800

    delete node

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * *insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to a dlistint_t linked list
+ * @idx: position where new node should be added starting from 0
+ * @n: data(n) for new node
+ * Return: the address of the new node or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t new;
+	dlistint_t temp;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->prev = NULL;
+	new->next = NULL;
+	new->n = n;
+	temp = *head;
+
+	if (!head)
+	{
+		if (idx == 0)
+		{
+			*h = new;
+			return (new);
+		}
+		return (NULL);
+	}
+	while (i < idx && temp)
+	{
+		i++;
+		temp = temp->next;
+	}
+	new->next = temp;
+	new->prev = temp->prev;
+	temp->prev->next = new;
+	temp->prev = new;
+	return (new);
+	}
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index -deletes the node at index of a
+ * dlistint_t linked list
+ * @head: pointer to dlistint_t linked list
+ * @index: position of node to be deleted in the index
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t tmp;
+	unsigned int i = 0;
+
+	if (!head)
+		return (-1);
+	tmp = *head;
+	if (tmp->prev == NULL && index == 0)
+	{
+		if (tmp->next == NULL)
+		{
+			free(tmp);
+			return (1);
+		}
+		tmp->next->prev = NULL;
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	while (tmp && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp->next == NULL && n == index)
+	{
+		tmp->prev->next = NULL;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		tmp->next->prev = tmp->prev;
+		tmp->prev->next = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	return (-1);
+}
